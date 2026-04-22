3d59bb9f7156da111bcb9eb737d13904aee5691d
v.svistunov <v.svistunov@corp.nekki.ru>
2026-04-22 23:14:02 +0300

    ребро вип

---
+++ environmental/Assets/Scripts/VillageGraph/Editor/MinimapEdgeRegistryEditor.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MinimapEdgeRegistry))]
+public class MinimapEdgeRegistryEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        EditorGUILayout.Space();
+        using (new EditorGUI.DisabledScope(serializedObject.isEditingMultipleObjects))
+        {
+            if (GUILayout.Button("Собрать все рёбра со сцены"))
+                CollectAllEdgesFromScene();
+        }
+    }
+
+    private void CollectAllEdgesFromScene()
+    {
+        var registry = (MinimapEdgeRegistry)target;
+        serializedObject.Update();
+
+        var edgesProp = serializedObject.FindProperty("edges");
+        if (edgesProp == null)
+        {
+            Debug.LogError("MinimapEdgeRegistry: не найдено поле edges.", registry);
+            return;
+        }
+
+        var found = new List<MinimapEdge>(Object.FindObjectsOfType<MinimapEdge>(true));
+        found.RemoveAll(e => e == null);
+        found.Sort((a, b) => string.CompareOrdinal(a.name, b.name));
+
+        Undo.RecordObject(registry, "Собрать рёбра MinimapEdgeRegistry");
+        edgesProp.ClearArray();
+        for (var i = 0; i < found.Count; i++)
+        {
+            edgesProp.InsertArrayElementAtIndex(i);
+            edgesProp.GetArrayElementAtIndex(i).objectReferenceValue = found[i];
+        }
+
+        serializedObject.ApplyModifiedProperties();
+        EditorUtility.SetDirty(registry);
+    }
+}
